74cbc65843beea8730fd41fb5e427de50c31afa0
Aghaz <aghaz2007a@gmail.com>
2026-07-16 14:35:02 +0530

    Add files via upload

---
+++ 01_basics/var_const.js
@@ -0,0 +1,14 @@
+// console.log("Hello World")
+let accountId = 12345
+let accountEmail = "user@example.com"
+let accountPassword = "securePassword123"
+let accountCity = "jaipur"
+
+
+console.log(accountId)
+console.log(accountEmail, accountPassword, accountCity)
+
+/* prefer not to use var 
+because of issue in block scope and functional loop 
+*/
+
