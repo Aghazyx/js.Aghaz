dc621ca6c964575f88a94653b079cb58d463d67a
Aghaz <aghaz2007a@gmail.com>
2026-07-16 20:49:24 +0530

    updated code

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,28 +1,50 @@
 // console.log("Hello world")
 
 // let score = 33;
-let score1 = "33abc"
+// let score1 = "33abc"
 // console.log(typeof(score));
 // console.log(typeof(score1));
 
-let valueInNumber = Number(score1);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// let valueInNumber = Number(score1);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
-//NaN in JavaScript stands for Not a Number.
+// //NaN in JavaScript stands for Not a Number.
 
-//"33abc" = NaN
-//true is 1 and false is 0
+// //"33abc" = NaN
+// //true is 1 and false is 0
 
-let isLoggedIn = 1 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// // let isLoggedIn = 1 
+// // let booleanIsLoggedIn = Boolean(isLoggedIn);
+// // console.log(booleanIsLoggedIn);
 
-// 1 == true && 0 == false 
-// " " == false
-// "Aghaz" == true 
+// // 1 == true && 0 == false 
+// // " " == false
+// // "Aghaz" == true 
 
-let someNumber = 33
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// // let someNumber = 33
+// // let stringNumber = String(someNumber);
+// // console.log(typeof stringNumber);
+// // console.log(stringNumber);
+
+// let str1 = " aghaz !"
+// let str2 = "bye aghaz"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2 );
+// console.log(1 + 2 + "2" );
+
+//learn a++ and ++a similiarly for --
+
+// console.log(2 > 1 < 3);
+
+// === strictly checks 
+
+// console.log("2" == 2 );
+// console.log("2" === 2 );
+
--- 01_basics/data_types.js
+++ 01_basics/data_types.js
@@ -1,13 +1,13 @@
-"use strict" ; //treats all js code as newer version of code 
+// "use strict" ; //treats all js code as newer version of code 
 
 // alert("Hello World") // we are using node js and not browser
 
  //code readability should be high 
 
-console.log("Hello World") 
-let name = " Aghaz "
-let age = 18
-let isLoggedIn = false
+// console.log("Hello World") 
+// let name = " Aghaz "
+// let age = 18
+// let isLoggedIn = false
 
 //number => 2 to power 53
 // bigint
@@ -19,15 +19,36 @@ let isLoggedIn = false
 
 //object 
 
-console.log(typeof name) //string
-console.log(typeof age) //number
-console.log(typeof isLoggedIn) //boolean
-console.log(typeof null) //object
-console.log(typeof undefined) //undefined
-console.log(typeof Symbol()) //symbol
-console.log(typeof {}) //object
-console.log(typeof []) //object
+// console.log(typeof name) //string
+// console.log(typeof age) //number
+// console.log(typeof isLoggedIn) //boolean
+// console.log(typeof null) //object
+// console.log(typeof undefined) //undefined
+// console.log(typeof Symbol()) //symbol
+// console.log(typeof {}) //object
+// console.log(typeof []) //object
 
 //the typeof syntax gives us the type of the variable we have discussed 
 
-console.log("bye bye")
+// console.log("bye bye")
+
+//primitive datatypes 
+// 7 catagories ; number, bigint, string, boolean, null, undefined, symbol
+
+//reference type also known as non primitive 
+
+// arrays , objects , functions
+ 
+const heroes =  [ "thor", "spiderman","iron man"]
+console.log(heroes);
+let myObj = {
+    name: "Aghaz",
+    age: 20,
+    isLoggedIn: false
+}
+console.log(myObj);
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(myFunction);
