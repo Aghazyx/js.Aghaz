f1967d611938f2b9e31623e33f8e60462d69905a
Aghaz <aghaz2007a@gmail.com>
2026-07-25 16:32:06 +0530

    Discussion of dates and times

--- 01_basics/dates.js
+++ 01_basics/dates.js
@@ -1,1 +1,22 @@
-console.log("this file is to discuss date and time in javascript")
+// console.log("this file is to discuss date and time in javascript")
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getFullYear());
+// console.log(myDate.getMonth());
+// console.log(myDate.getDate());
+// console.log(myDate.getHours());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getSeconds());
+
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date('2023-01-01');
+// console.log(myCreatedDate.toString());
+// let myTimeStamp = myCreatedDate.getTime();
+// console.log(myTimeStamp);
+
+// let currentDate = Date.now();
+// console.log(currentDate.toString());
+
