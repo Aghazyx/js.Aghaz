6015d0f2eebf8103b175ba03aabd196dd78b83e5
Aghaz <aghaz2007a@gmail.com>
2026-07-25 14:23:21 +0530

    Discussion on numbers and maths

--- 01_basics/nums_and_maths.js
+++ 01_basics/nums_and_maths.js
@@ -1,1 +1,9 @@
-console.log("hellow everyone , this is a new file where discussion on numbers and maths will be done");
+// console.log("hellow everyone , this is a new file where discussion on numbers and maths will be done");
+const score = 40 
+const balance = new Number(100.12565783)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const othernumber = 23.1234558374
+console.log(othernumber.toPrecision(3));
