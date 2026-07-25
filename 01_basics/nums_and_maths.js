1f7a75653c9aab3338a172446b74c4f94f3b52db
Aghaz <aghaz2007a@gmail.com>
2026-07-25 15:52:31 +0530

    Discussion on numbers and maths

--- 01_basics/nums_and_maths.js
+++ 01_basics/nums_and_maths.js
@@ -1,9 +1,29 @@
 // console.log("hellow everyone , this is a new file where discussion on numbers and maths will be done");
-const score = 40 
-const balance = new Number(100.12565783)
-console.log(balance)
-console.log(balance.toString().length)
-console.log(balance.toFixed(2))
+// const score = 40 
+// const balance = new Number(100.12565783)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
 
-const othernumber = 23.1234558374
-console.log(othernumber.toPrecision(3));
+// const othernumber = 23.1234558374
+// console.log(othernumber.toPrecision(3));
+
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++Maths++++++
+// console.log('Maths')
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.abs(-4.6));
+// console.log(Math.min(4, 2, 8, 6));
+// console.log(Math.max(4, 2, 8, 6));
+// console.log(Math.random());
+// console.log((Math.random()*10) +1 );
+
+const max = 20 
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
