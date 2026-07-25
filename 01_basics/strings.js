7c7fead0a56b49f579316802bc5477df4a32f43c
Aghaz <aghaz2007a@gmail.com>
2026-07-25 13:58:13 +0530

    Discussion on string

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,4 +1,35 @@
 // console.log("Hello World");
 //this is just a testing line 
 
-console.log("i am back after a short trip to north bengal and sikkim,HELLYEAH!")
+// console.log("i am back after a short trip to north bengal and sikkim,HELLYEAH!")
+
+// const name = "hitesh"
+// const repoCount = 50 
+//  console.log(name + repoCount + "Value")
+// console.log(`hellow my name is ${name} and my repo count is ${repoCount}`)
+
+// console.log(typeof name ) 
+const gameName = new String('Valorant')
+console.log(gameName[5])
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(5))
+console.log(gameName.indexOf('a'))
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,-5)
+console.log(anotherString);
+
+
+const trialstring = "       aghaz        "
+console.log(trialstring)
+console.log(trialstring.trim())
+
+const url = "https://www.example javascript.com"
+console.log(url)
+console.log(url.replace(' ','-'))
+console.log(url.includes('javascript'))
+console.log(url.startsWith('https'))
+console.log(gameName.italics())
