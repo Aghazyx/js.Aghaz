435aa7f08c8133d0bd156ac36f66f8d307248da1
Aghaz <aghaz2007a@gmail.com>
2026-07-27 02:25:51 +0530

    dicussion on Arrays

--- 01_basics/arrays.js
+++ 01_basics/arrays.js
@@ -32,10 +32,36 @@ const myArr = [0,1,2,3,4,5,6,7,8,9,];
 //slice and spice 
 
 // console.log("A",myArr)
-const myn1 = myArr.slice(1,3)
-console.log(myn1)
-// console.log("B",myArr)
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1)
+// // console.log("B",myArr)
 
 
-const myn2 = myArr.splice(1,3)
-console.log(myn2)
+// const myn2 = myArr.splice(1,3)
+// console.log(myn2)
+
+// const marvel = ["iron man","thor","dr doom"]
+// const dc = ["batman","superman","wonder woman"]
+
+// marvel.push(dc)
+// console.log(marvel)
+
+// const allHeroes = marvel.concat(dc)
+
+// const allHeroes = [...marvel,...dc]
+// console.log(allHeroes)
+
+// const realArr = [1,2,[3,4,5,6],7,8,9,10,11,[12,13,14,15],16,17,[18,19],20]
+// const realArr2 = realArr.flat(Infinity)
+// console.log(realArr2)
+
+// console.log(Array.isArray("AGHAZZAHINRAHAMAN"))
+// console.log(Array.from("AGHAZZAHINRAHAMAN"))
+// console.log(Array.isArray([1,2,3,4,5,6,7,8,9]))
+
+
+// let score1 =100
+// let score2 =200
+// let score3 =300
+
+// console.log(Array.of(score1,score2,score3))
