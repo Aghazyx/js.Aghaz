df7b935d19fa8adb5bd193e4572c989feb3fe172
Aghaz <aghaz2007a@gmail.com>
2026-07-27 01:26:34 +0530

    dicussion on Arrays

--- 01_basics/arrays.js
+++ 01_basics/arrays.js
@@ -1,1 +1,41 @@
-console.log('hello everyone , we are going to discuss about arrays in this file')
+// console.log('hello everyone , we are going to discuss about arrays in this file')
+
+const myArr = [0,1,2,3,4,5,6,7,8,9,];
+// console.log(myArr);
+// console.log(myArr[0]);
+
+//array methods 
+
+// myArr.push(1000);
+// console.log(myArr);
+
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(11);
+// console.log(myArr);
+
+// console.log(myArr.shift());
+
+
+// console.log(myArr.includes("Aghaz"))
+// console.log(myArr.indexOf(5))
+
+// const myArr2 = [10, 11, 12, 13, 14, 15];
+// console.log(myArr2);
+
+// const newArr = myArr2.join()
+// console.log(typeof myArr2)
+// console.log(newArr)
+
+
+//slice and spice 
+
+// console.log("A",myArr)
+const myn1 = myArr.slice(1,3)
+console.log(myn1)
+// console.log("B",myArr)
+
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2)
