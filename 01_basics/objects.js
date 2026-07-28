824654b88d2b44445ce8cecd064b0f79f238eb56
Aghaz <aghaz2007a@gmail.com>
2026-07-28 17:46:48 +0530

    dicussion on objects

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -2,15 +2,15 @@
 //object.create
 //object lieterals 
 
-const mySym = Symbol("Key1")
+// const mySym = Symbol("Key1")
 
-const jsUser = {
-    name: "Aghaz",
-    [mySym]: "Value1",  
-    age: 20,
-    email: "john@example.com",
-    favouriteColor: "Red"
-}
+// const jsUser = {
+//     name: "Aghaz",
+//     [mySym]: "Value1",  
+//     age: 20,
+//     email: "john@example.com",
+//     favouriteColor: "Red"
+// }
 // console.log(jsUser);
 
 
@@ -35,8 +35,57 @@ const jsUser = {
 // console.log(jsUser)
 // console.log(typeof jsUser)
 
-jsUser.greeting = function(){
-    console.log("hellow coders")
-}
-console.log(jsUser.greeting())
+// jsUser.greeting = function(){
+//     console.log(`hellow js user, ${this.name}`)
+// }
+// console.log(jsUser.greeting())
+
+
+// const tinderUser =  new Object()
+// tinderUser.id = "123abc"
+// tinderUser.name = "Santiago"
+// tinderUser.isLOggedin = false
+// console.log(tinderUser)
+
+// const regularUser =  {
+//     email : "aghaz1234@gmail.com",
+//     fullName : {
+//         firstName : "aghaz",
+//         lastName : "zahin rahaman"
+//     }
+// // }
+// console.log(regularUser.fullName.firstName)
+
+// const obj1 = {1 : "a",2: "e"}
+// const obj2 = {4 : "i",6: "f"}
+// const obj3 = {5 : "h",7: "g"}
+// const obj4 = {obj1,obj2,obj3}
+// // console.log(obj4.obj1)
+// const obj5 = {...obj1,...obj2}
+// // const obj5 = Object.assign({},obj1,obj2,obj3)
+// console.log(obj5)
+
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+// console.log(tinderUser.hasOwnProperty('islogged'))
+
+
+
+// const course = {
+//     coursename : 'jsHindi',
+//     price : '999',
+//     courseDurcation : '6 months',
+
+// }
+// console.log(course)
+
+//API : Application Programming Interface
+
+// {
+//     name : "Aghaz",
+//     course : "JS in Hindi",
+//     price : "free",
+// }
 
