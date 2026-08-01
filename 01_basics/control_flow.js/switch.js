84810a07962754313ea6489b8af500b5064fa4eb
Aghaz <aghaz2007a@gmail.com>
2026-08-01 12:39:33 +0530

    Add new JavaScript exercises on control flow

---
+++ 01_basics/control_flow.js/switch.js
@@ -0,0 +1,27 @@
+// switch(key){
+//     case value1:
+//          code to be executed if key === value1
+//         break;
+//     case value2:
+//          code to be executed if key === value2
+//         break;
+//     default:
+//          code to be executed if key !== value1 && key !== value2
+//          break;
+// }
+
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    default:
+        console.log("Invalid month");
+        break;
+}
