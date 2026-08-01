5046e7a569720011d961c2d9c538e8a1208d6602
Aghaz <aghaz2007a@gmail.com>
2026-08-01 12:35:50 +0530

    Add new JavaScript exercises on control flow

---
+++ 01_basics/control_flow.js/one.js
@@ -0,0 +1,59 @@
+//if statement
+//    if (condition) {
+//        code to be executed if condition is true
+//    } else {
+//        code to be executed if condition is false
+//    }
+
+// const userLoggedIn = true;
+// if (userLoggedIn != true) {
+//        console.log("User is not logged in");
+// } else {
+//        console.log("User is logged in");
+// }
+
+
+// const temperature = 25;
+// if (temperature > 30) {
+//     console.log("It's a hot day");
+// } else {
+//     console.log("It's not a hot day");
+// }
+// console.log("temperature is less than 30 , hella cool ahh weather")
+
+
+// const score = 20
+// if( score > 100) {
+//     const power = "fly"
+//     console.log(`user power : ${power}`)
+// } else {
+//     console.log("user power : none")
+// }
+
+
+// let: Use let when the value will change.
+// const: Use const when the value should not be reassigned.
+
+
+// const balance = 1000
+// if (balance > 500) console.log("You have enough balance");
+// console.log("You can make a purchase"); // this is not a good practice of coding 
+
+
+// const balance = 1000
+// if (balance < 500) {
+//     console.log("You don't have enough balance");
+// } else if (balance >= 500 && balance < 1000) {
+//     console.log("You have enough balance");
+// } else {
+//     console.log("You have a high balance");
+// }
+
+// const userLoggedIN = true ;
+// const debitCard = true;
+
+// if (userLoggedIN && debitCard) {
+//     console.log("User can make a purchase");
+// } else {
+//     console.log("User cannot make a purchase");
+// }
