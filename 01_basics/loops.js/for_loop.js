bf7d3464a11380eafed87ea5769d15441765ca3f
Aghaz <aghaz2007a@gmail.com>
2026-08-01 16:30:38 +0530

    for loop and continue and break statement

---
+++ 01_basics/loops.js/for_loop.js
@@ -0,0 +1,32 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log("Iteration:", i);
+// }
+
+// for(let index = 0 ; index <= 10;index++){
+//     const element = index;
+//     if( element == 5){
+//         console.log("Found element:", element , "at index:", index);
+//     }
+//     console.log("Current element:", element);
+// }
+
+// for ( let i = 1 ; i <= 10 ; i++){
+//     console.log("OUTER LOOP:", i);
+//     for(let j = 0 ; j <= 10 ; j++){
+//         console.log(i + "*" + j + ":", i*j);
+//     }
+// }
+
+
+// for( let index = 0 ; index <= 20 ; index ++){
+
+//     //     if ( index === 2){
+//     //     continue;
+//     // }
+//     //     if ( index === 5){
+//     //     break;
+//     // }
+
+//     console.log("I love you")
+//     console.log("Current index:", index);
+// }
