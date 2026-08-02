5aac8823e103ba9f3dc916ec36d39c0a5f0d2a09
Aghaz <aghaz2007a@gmail.com>
2026-08-02 14:56:05 +0530

    while and do while loop

---
+++ 01_basics/loops.js/do_while_loop.js
@@ -0,0 +1,18 @@
+// let index = 0 
+// while(index < 5) {
+//   console.log(index + 2);
+//   index++;
+// }   
+
+// let myArray = ["thor","spiderman","ironman"]
+// let arr = 0 
+// while ( arr < myArray.length){
+//     console.log(`Superhero is ${myArray[arr]}`)
+//     arr++;
+// }
+
+// let score = 1
+// do {
+//   console.log(score);
+//   score++;
+// } while (score <= 5);
