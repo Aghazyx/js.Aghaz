bf6258992d60acdfb131673d890c5504db27d9c8
Aghaz <aghaz2007a@gmail.com>
2026-08-02 15:36:39 +0530

    for of loop and little discussion on mapping

---
+++ 01_basics/loops.js/moreAboutLoops.js
@@ -0,0 +1,17 @@
+// const myObject = { 
+//     js : "javascript",
+//     html : "hypertext markup language",
+//     css : "cascading style sheets",
+//     react : "react library",
+//     cpp : "c plus plus",
+//     py : "python"
+// }
+
+// for (const key in myObject){
+//     console.log(key ,"shortcut is for : ", myObject[key]);
+// }
+// const programming = ["js","html","css","react","cpp","py"]
+// for (const key in programming) {
+//   console.log(programming[key]);
+// }
+
